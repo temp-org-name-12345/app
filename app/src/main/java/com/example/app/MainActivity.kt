d0169a770377917c51511b497c0d74c143c2d391
rivercity310 <h970126@gmail.com>
2023-09-09 14:14:39 +0900

    #1 - Map Setting

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -18,8 +18,10 @@ import androidx.compose.ui.viewinterop.AndroidView
 import com.example.app.ui.theme.AppTheme
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.MapLifeCycleCallback
 import com.kakao.vectormap.MapView
+import com.kakao.vectormap.MapViewInfo
 import java.lang.Exception
 
 class MainActivity : ComponentActivity() {
@@ -39,6 +41,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// KAKAO MAP DOCS : https://apis.map.kakao.com/android_v2/docs/getting-started/quickstart/
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -47,19 +50,56 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             AndroidView(
                 factory = { context: Context ->
                     val mv = MapView(context)
-                    mv.start(object : MapLifeCycleCallback() {
-                        override fun onMapDestroy() {
-                            TODO("Not yet implemented")
-                        }
+                    mv.start(
+                        object : MapLifeCycleCallback() {
+                            // 지도 API가 정상적으로 종료될 때 호출
+                            override fun onMapDestroy() {
+                                TODO("Not yet implemented")
+                            }
 
-                        override fun onMapError(error: Exception?) {
-                            TODO("Not yet implemented")
+                            // 인증 실패 및 지도 사용 중 에러 발생시 호출
+                            override fun onMapError(error: Exception?) {
+                                TODO("Not yet implemented")
+                            }
+                        },
+
+                        object : KakaoMapReadyCallback() {
+                            // 인증 후 API가 정상적으로 실행될 때 호출
+                            override fun onMapReady(kakaoMap: KakaoMap) {
+                                Log.e("Map", "지도 시작")
+                            }
+
+                            // 지도 시작 시 확대/축소 줌 레벨 설정
+                            override fun getZoomLevel(): Int {
+                                return super.getZoomLevel()
+                            }
+
+                            // 지도 시작 시 위치 좌표 설정
+                            override fun getPosition(): LatLng {
+                                return LatLng.from(37.486960, 127.115587)
+                            }
+
+                            // 지도 시작 시 App 및 MapType 설정
+                            override fun getMapViewInfo(): MapViewInfo {
+                                return super.getMapViewInfo()
+                            }
+
+                            // KakaoMap의 고유한 이름 설정
+                            override fun getViewName(): String {
+                                return super.getViewName()
+                            }
+
+                            // KakaoMap의 tag를 설정
+                            override fun getTag(): Any {
+                                return super.getTag()
+                            }
+
+                            // 지도 시작 시 visible 여부 설정
+                            override fun isVisible(): Boolean {
+                                return true
+                            }
                         }
-                    }, object : KakaoMapReadyCallback() {
-                        override fun onMapReady(kakaoMap: KakaoMap) {
-                            Log.e("Map", "지도 시작")
-                        }
-                    })
+                    )
 
                     mv
                 },
